ca7d556b7475d8b1f21841b55e64fa48e506250c
Raima Saha <you@example.com>
2026-06-13 11:06:39 -0400

    added lilypads to signify step progress

--- script.js
+++ script.js
@@ -5,26 +5,36 @@ const svgContainer = document.getElementById("flower");
 
 
 // -------------- DESIGN CONSTANTS ----------------- //
-
+let globalMaxDistance;
+const stepGoal = 10000; // according to my suunto watch
 const petalPath = "M 0,0 C 30,60 30,60 0,100 C -30,60 -30,60 0,0";
 const leafPath = "M 0,0 C 10,-90 70,-40 100,-110 C 100,-40 70,20 0,0";
 const objectSize = d3.scaleLinear() // steps
-        .domain([0, 12000])
-        .range([0.2, 0.3])
-        .clamp(true)
-        .unknown(0.05);
+    .domain([0, 12000])
+    .range([0.2, 0.3])
+    .clamp(true)
+    .unknown(0.05);
+
+
+
+// const lilypadAngle = d3.scaleLinear()
+//         .domain(0, 10000)
+//         .range(0, Math.PI * 2)
+//         .clamp(false)
+//         .unknown(0);
+
 
 const objectOpacity = d3.scaleLinear() // distance
-        .domain([0, 10000])
-        .range([0.3, 1]);
+    .domain([0, 10000])
+    .range([0.3, 1]);
 
 const flowerColor = d3.scaleSequential(d3.interpolatePlasma) // distance 
-        .domain([0, 10000])
-        .unknown("#dddff0");
+    .domain([0, 10000])
+    .unknown("#dddff0");
 
 const leafColor = d3.scaleSequential(d3.interpolateYlGnBu) // distance 
-        .domain([0, 10000])
-        .unknown("#dddff0");
+    .domain([0, 10000])
+    .unknown("#dddff0");
 
 const CELL_SIZE = 44;
 const PADDING = 30;
@@ -74,41 +84,95 @@ function drawFlower(x, y, options) {
 }
 
 function drawBuds(x, y) {
+    const budColor = "#1e32eb"
     const circle = document.createElementNS(svgNS, "circle");
     circle.setAttribute("cx", x);
     circle.setAttribute("cy", y);
     circle.setAttribute("r", "4");
-    circle.setAttribute("fill", "steelblue");
+    circle.setAttribute("fill", budColor);
     svgContainer.appendChild(circle);
 }
 
 function drawBudsWithRings(x, y, intensity) {
+    const budColor = "#1e32eb"
+    const ringColor = "#1e32eb";
     const ringGroup = document.createElementNS(svgNS, "g");
     const circle = document.createElementNS(svgNS, "circle");
     circle.setAttribute("cx", x);
     circle.setAttribute("cy", y);
     circle.setAttribute("r", "4");
-    circle.setAttribute("fill", "steelblue");
+    circle.setAttribute("fill", budColor);
     svgContainer.appendChild(circle);
     ringGroup.setAttribute("transform", `translate(${x}, ${y})`);
     for (let i = 0; i < intensity; i++) {
         const circle = document.createElementNS(svgNS, "circle");
         circle.setAttribute("cx", 0);
         circle.setAttribute("cy", 0);
-        circle.setAttribute("r", 8+5*i);
+        circle.setAttribute("r", 8 + 5 * i);
         circle.setAttribute("fill", "none")
-        circle.setAttribute("stroke", "steelblue");
+        circle.setAttribute("stroke", ringColor);
         ringGroup.appendChild(circle);
     }
     svgContainer.appendChild(ringGroup);
 }
 
-function drawLilypads(x, y, options) {
+// opacityDimmed == true  -> pad rendered faded (used behind a flower)
+// opacityDimmed == false -> pad rendered solid (standalone)
+function drawLilypads(x, y, opacityDimmed, options) {
+    const backgroundColor = opacityDimmed ? "#fefec169" : "#fefec1fd";
+    const mainColor = opacityDimmed ? "#65ad3e6e" : "#65ad3e";
+    const overlapColor = opacityDimmed ? "#06420e6f" : "#06420e";
+    const lilypadRadius = opacityDimmed ?  6 : options.lilypadRadius;
+    const completion = options.completion;
 
+    const baseCompletion = Math.min(completion, 1);
+    const excessCompletion = Math.max(0, Math.min(completion - 1, 1));
+
+    const padGroup = document.createElementNS(svgNS, "g");
+    const circle = document.createElementNS(svgNS, "circle");
+    
+    circle.setAttribute("cx", 0);
+    circle.setAttribute("cy", 0);
+    circle.setAttribute("r", lilypadRadius);
+    circle.setAttribute("fill", backgroundColor);
+    padGroup.appendChild(circle);
+    padGroup.setAttribute("transform", `translate(${x}, ${y})`);
+
+    if (baseCompletion > 0) {
+        const achievementArc = d3.arc()
+            .innerRadius(0)
+            .outerRadius(lilypadRadius)
+            .startAngle(0)
+            .endAngle(-Math.PI * 2 * baseCompletion);
+
+        const arcPath = document.createElementNS(svgNS, "path");
+        arcPath.setAttribute("d", achievementArc());
+        arcPath.setAttribute("fill", mainColor);
+        padGroup.appendChild(arcPath);
+    }
+
+    if (excessCompletion > 0) {
+        const excessArc = d3.arc()
+            .innerRadius(0)
+            .outerRadius(lilypadRadius)
+            .startAngle(0)
+            .endAngle(-Math.PI * 2 * excessCompletion);
+
+        const arcPath = document.createElementNS(svgNS, "path");
+        arcPath.setAttribute("d", excessArc());
+        arcPath.setAttribute("fill", overlapColor);
+        padGroup.appendChild(arcPath);
+    }
+    svgContainer.appendChild(padGroup);
 }
 
-function lilypadOptionsForDay() {
-    
+function lilypadOptionsForDay(day, lilypadRadius) {
+    const rawCompletion = day.steps != null ? day.steps / stepGoal : 0;
+    return {
+        lilypadRadius: lilypadRadius(day.distance),
+        completion: rawCompletion
+    }
+
 }
 
 function flowerOptionsForDay(day) {
@@ -153,6 +217,13 @@ d3.json("combined.json").then(rawData => {
     }));
     console.log(data);
 
+    globalMaxDistance = d3.max(data, d => d.distance);
+    const lilypadRadius = d3.scaleLinear()
+        .domain([0, globalMaxDistance])
+        .range([5, 20])
+        .clamp(false)
+        .unknown(0);
+
     // data.slice(140, 150).forEach((day, i) => {
     //     const x = 100 + i * 80;
     //     const y = 200;
@@ -163,7 +234,7 @@ d3.json("combined.json").then(rawData => {
     //     }
     // });
 
-    const startDate = data[0].date; 
+    const startDate = data[0].date;
     data.forEach((day, i) => {
         const { x, y } = positionForDate(startDate, day.date);
         if (day.cycleId != null) {
@@ -171,23 +242,21 @@ d3.json("combined.json").then(rawData => {
                 drawBudsWithRings(x, y, day.intensity);
             } else {
                 drawFlower(x, y, flowerOptionsForDay(day));
+                drawLilypads(x, y, true, lilypadOptionsForDay(day, lilypadRadius));
+
             }
         } else {
             if (day.steps == null && day.distance == null) {
                 drawBuds(x, y);
             } else {
-                if (i >= 1 && day.steps > data[i-1].steps && day.distance > data[i-1].distance) { // today > yesterday -> normal leaf
-                    drawLeaf(x, y, true, leafOptionsForDay(day));
-                } else if (i >= 1 && day.steps < data[i-1].steps && day.distance < data[i-1].distance) { // today < yesterday -> reflect leaf
-                    // drawLeaf(x, y, false, leafOptionsForDay(day));
-                    drawLeaf(x, y, true, leafOptionsForDay(day));
-                }
-                drawLeaf(x, y, true, leafOptionsForDay(day));
+
+                drawLilypads(x, y, false, lilypadOptionsForDay(day, lilypadRadius));
             }
         }
     });
 
-    svgContainer.attr("background", "black");
+
+    // svgContainer.attr("background", "black");
 
     // const legend = d3.select("#flower")
     //                 .append("svg")
